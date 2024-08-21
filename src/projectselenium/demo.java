1324358b91059f0a9298b0b534572817cfe6717a
Swaraj Bandhu Prasad Kalwar <swarajbandhi0000@gmail.com>
2024-08-21 16:37:09 +0530

    done with some findelement function in selenium webdriver by using xpath and id

--- src/projectselenium/demo.java
+++ src/projectselenium/demo.java
@@ -1,5 +1,6 @@
 package projectselenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -8,9 +9,14 @@ public class demo {
 	WebDriver driver;
 	public void openBrowser() {
         driver = new EdgeDriver();
-        driver.get("https://practice.expandtesting.com/login");
+        driver.get("https://google.com");
+        driver.findElement(By.id("APjFqb")).sendKeys("https://practice.expandtesting.com/login/");
+        driver.findElement(By.id("APjFqb")).submit();
+        driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3")).click();
+        driver.findElement(By.id("username")).sendKeys("practice");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//*[@id=\"login\"]/button")).click();
 	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
