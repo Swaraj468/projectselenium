6d9f447fb975139450d8c528d2bd96707b6ea0fd
Swaraj Bandhu Prasad Kalwar <swarajbandhi0000@gmail.com>
2024-08-21 15:48:35 +0530

    tried some sites opening automation with the help of selenium webdriver

--- src/projectselenium/demo.java
+++ src/projectselenium/demo.java
@@ -1,9 +1,21 @@
 package projectselenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class demo {
+	
+	WebDriver driver;
+	public void openBrowser() {
+        driver = new EdgeDriver();
+        driver.get("https://practice.expandtesting.com/login");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		demo obj = new demo();
+		obj.openBrowser();
 
 	}
 
